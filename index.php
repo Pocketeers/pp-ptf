1b5c6896c2e5ea82bc50a833061f247a1b204b2b
Peeal <piyal01713@gmail.com>
2016-05-06 00:33:01 +0800

    searchbar update

--- index.php
+++ index.php
@@ -26,48 +26,55 @@ include'getnavigation.php';
 
 
   <div class="middlebody">
-      <h3 class="main-message">You can find part-time and full-time jobs on workwork. So get off the couch and go make some money! </h3>
+    <h3 class="main-message">You can find part-time and full-time jobs on workwork. So get off the couch and go make some money! </h3>
 
 
     <div class="container">
       <!-- Search bar -->
-      <form class="form-inline" action="index.php" method="post" style="text-align: center;">
-        <div clas="form-group">
-            <input type="text" name="search" class="form-control" id="exampleInputName2"  placeholder="Search">
-     
-                  <!-- setting text area box size rows= height, cols= width, maxlength= char limit 
-                   -->   
-                   <select class="form-control" name="jobcat">
-                    <option value="">Choose a Category</option>
-                    <option value="services">Services</option>
-                    <option value="food">Food</option>
-                  </select> 
-            <button type="submit" class="bt btn-primary">Search</button>
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <form class="form-inline" action="index.php" method="post">
+              <div class="form-group">
+                <input type="text" name="search" class="form-control"  placeholder="Search">
+              </div>
+
+              <div class="form-group">  
+                <select class="form-control" name="jobcat">
+                  <option value="">Choose a Category</option>
+                  <option value="services">Services</option>
+                  <option value="food">Food</option>
+                </select> 
+              </div>
+
+              
+
+              <button type="submit" class="btn btn-primary">Search</button>
+
+            </form>
         </div>
-            
-      </form>
-
-         
-      
-      
-
-      <div id="job-list" class="col-md-8 col-md-offset-2">
-        <hr>
-        
-        <?php include'getindex.php'; ?>
-          <div id="pagination_controls"><?php echo $paginationCtrls; ?></div>
-
       </div>
-    </div>
 
-  </div>
 
-  <?php
+
+
+
+            <div id="job-list" class="col-md-8 col-md-offset-2">
+              <hr>
+
+              <?php include'getindex.php'; ?>
+              <div id="pagination_controls"><?php echo $paginationCtrls; ?></div>
+
+            </div>
+          </div>
+
+        </div>
+
+        <?php
   //include scripts php file
-  include'_scripts.php';
-  include'_footer.php';
-  ?>
+        include'_scripts.php';
+        include'_footer.php';
+        ?>
 
-</body>
+      </body>
 
-</html>
+      </html>
